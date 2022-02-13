2ab0c25ad6d27892a04158c95b3b530917dbd623
Akuli <akuviljanen17@gmail.com>
2022-02-13 18:33:23 +0200

    saving

--- index.js
+++ index.js
@@ -3,12 +3,33 @@ document.addEventListener("DOMContentLoaded", () => {
 
   let dbDoc = null;
 
+  async function saveToDb() {
+    console.log("Saving begins...");
+    const [toBuyList, boughtList] = document.querySelectorAll(".itemList");
+    await dbDoc.set({
+      toBuy: [...toBuyList.children].map(item => item.querySelector("span").textContent),
+      bought: [...boughtList.children].map(item => item.querySelector("span").textContent),
+    });
+    console.log("Saving done");
+  }
+
   function beginTextEditing(item) {
     item.querySelector("span").style.display = "none";
     item.querySelector("input").style.display = "";
     item.querySelector("input").value = item.querySelector("span").textContent;
     item.querySelector("input").select();
     window.setTimeout(() => item.querySelector("input").focus(), 1);
+  }
+
+  function endTextEditing(item) {
+    item.querySelector("input").style.display = "none";
+    item.querySelector("span").style.display = "";
+
+    const newText = item.querySelector("input").value.trim();
+    if (newText) {
+      item.querySelector("span").textContent = newText;
+      saveToDb();
+    }
   }
 
   let draggedItem = null;
@@ -20,30 +41,20 @@ document.addEventListener("DOMContentLoaded", () => {
     item.innerHTML = '<span></span><input style="display:none;" /><button>X</button>';
 
     item.querySelector("span").onclick = () => beginTextEditing(item);
-
-    // Text editing is done when unfocusing
-    function endTextEditing() {
-      item.querySelector("input").style.display = "none";
-      item.querySelector("span").style.display = "";
-
-      const newText = item.querySelector("input").value.trim();
-      if (newText) {
-        item.querySelector("span").textContent = newText;
-      }
-    }
-    item.querySelector("input").addEventListener("focusout", endTextEditing);
-    item.querySelector("input").addEventListener("keyup", e => {if(e.key==="Enter") endTextEditing()});
+    item.querySelector("input").addEventListener("focusout", () => endTextEditing(item));
+    item.querySelector("input").addEventListener("keyup", e => {if(e.key==="Enter") event.target.blur()});
 
     // Delete when X button is clicked
     item.querySelector("button").onclick = () => {
       const text = item.querySelector("span").textContent;
       if (confirm(`Delete item "${text}"?`)) {
         item.remove();
+        saveToDb();
       }
     };
 
     item.addEventListener("dragstart", () => { draggedItem = item; item.classList.add("dragged"); });
-    item.addEventListener("dragend", () => item.classList.remove("dragged"));
+    item.addEventListener("dragend", () => { item.classList.remove("dragged"); saveToDb(); });
 
     return item;
   }
@@ -76,6 +87,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // FIXME: create doc if not exists
     dbDoc = firebase.firestore().collection("lists").doc(firebase.auth().getUid());
+    window.dbDocForDebugging = dbDoc;  // TODO: remove
 
     const data = (await dbDoc.get()).data();
     const [toBuyList, boughtList] = document.querySelectorAll(".itemList");
@@ -93,5 +105,6 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     document.getElementById("newItemButton").disabled = false;
+    document.getElementById("loadingOverlay").style.display = 'none';
   });
 });
