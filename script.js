2e1bf84e5827fa911bcb30f730a104902029b75f
Akuli <akuviljanen17@gmail.com>
2022-02-13 13:10:57 +0200

    delete button

--- script.js
+++ script.js
@@ -2,14 +2,35 @@ document.addEventListener("DOMContentLoaded", () => {
   function addItem(alreadyBought) {
     const div = document.createElement("div");
     div.classList.add("item");
-    div.innerHTML = "<span></span><button>X</button>";
+    div.innerHTML = '<span></span><input style="display:none;" /><button>X</button>';
     document.querySelectorAll(".itemList")[+!!alreadyBought].appendChild(div);
+
+    // Edit text when clicked
+    div.querySelector("span").onclick = () => {
+      div.querySelector("span").style.display = "none";
+      div.querySelector("input").style.display = "";
+      div.querySelector("input").value = div.querySelector("span").textContent;
+      div.querySelector("input").select();
+      div.querySelector("input").focus();
+    };
+
+    // Text editing is done when unfocusing
+    function endTextEditing() {
+      div.querySelector("input").style.display = "none";
+      div.querySelector("span").style.display = "";
+      div.querySelector("span").textContent = div.querySelector("input").value;
+    }
+    div.querySelector("input").addEventListener("focusout", endTextEditing);
+    div.querySelector("input").addEventListener("keyup", e => {if(e.key=="Enter") endTextEditing()});
+
+    // Delete when X button is clicked
     div.querySelector("button").onclick = () => {
       const text = div.querySelector("span").textContent;
       if (confirm(`Delete item "${text}"?`)) {
         div.remove();
       }
     };
+
     return div;
   }
 
